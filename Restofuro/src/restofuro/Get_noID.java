a137a2f621d6752d1bf688eeb0aa99d13c805fde
lai2 <informatica@lai2>
2020-03-04 08:57:39 +0100

    final

--- Restofuro/src/restofuro/Get_noID.java
+++ Restofuro/src/restofuro/Get_noID.java
@@ -1,4 +1,5 @@
 package restofuro;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,12 +7,13 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-public class RestFulClass {
-    
-    
-    
-    
-    public void Get(){
+
+/**
+ *
+ * @author informatica
+ */
+public class Get_noID {
+       public void Get(){
         try {
 
 		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
@@ -167,6 +169,50 @@ public class RestFulClass {
 
 	 }
 
-	}
+    }
+    public void Post(){
+        try {
+
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+
+		String input = "{\n" +"  \"employeeId\": 874,\n" +
+                            "  \"firstName\": \"Shrek\",\n" +
+                            "  \"lastName\": \"Of the swamp\",\n" +
+                            "  \"email\": \"Shrek@swamp.mine\",\n" +
+                            "  \"phone\": \"123456789\"\n" +
+                            "}";
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+
+		if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+			throw new RuntimeException("Failed : HTTP error code : "
+				+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	 }
     }
 }
